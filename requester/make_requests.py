7de7e6310ea82a6a7fe4f357b7f0c58ab5788bba
BielStela <biel.stela@gmail.com>
2018-05-07 21:49:36 +0200

    aded get_params request to requester

--- requester/make_requests.py
+++ requester/make_requests.py
@@ -26,16 +26,25 @@ def request_with_uri():
     print(req.url)
     print(req.text)
 
+def get_params():
+    uri = "http://padel:5000/padel/api/v0.1/get_params"
+    req = requests.get(uri)
+    print(req.json())
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--uri', action='store_true')
     group.add_argument('--json', action='store_true')
+    group.add_argument('--params', action='store_true')
 
     args = parser.parse_args()
     if args.uri:
         request_with_uri()
 
-    if args.json:
+    elif args.json:
         request_with_json()
+
+    elif args.params:
+        get_params()
+    
